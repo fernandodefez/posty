ab7d365a98611b0ac926c3b1ebc6a369ff7aef75
Fernando <fernandodefez@outlook.com>
2022-10-07 13:49:48 -0500

    Post's Requests (Store and Update) Were Created

---
+++ app/Http/Requests/Post/UpdatePostRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Post;
+
+use App\Models\Post;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $post = Post::find($this->route('post'))[0];
+        return $post && $this->user()->can('update', $post);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'body' => [
+                'required',
+                'max:350'
+            ]
+        ];
+    }
+}
