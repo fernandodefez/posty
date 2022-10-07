14434deb123d8d0769cede05673ed0910823defb
Fernando <fernandodefez@outlook.com>
2022-10-07 13:47:58 -0500

    Laravel Fortify Actions

---
+++ app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param array $input
+     * @return User
+     * @throws ValidationException
+     */
+    public function create(array $input)
+    {
+        Validator::make($input, [
+            'name' => ['required', 'string', 'alpha', 'min:3', 'max:255'],
+            'username' => [
+                'required',
+                'string',
+                'alpha',
+                'min:3',
+                'max:255',
+                'regex:/^\S*$/u',
+                Rule::unique(User::class)
+            ],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique(User::class),
+            ],
+            'password' => $this->passwordRules(),
+        ])->validate();
+
+        return User::create([
+            'name' => $input['name'],
+            'username' => $input['username'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password']),
+        ]);
+    }
+}
