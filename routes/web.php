5694121adb7eb609d960217165feb296d0cb5b1a
Fernando <fernandodefez@outlook.com>
2022-10-07 13:42:45 -0500

    Added Web Endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Web\Post\Like\LikeController;
+use App\Http\Controllers\Web\Post\PostController;
+use App\Http\Controllers\Web\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/api/test', function() {
+    return view('api');
+})->name('api.test');
+
+Route::get('/', function() {
     return view('welcome');
-});
+})->name('index');
+
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home');
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+Route::get('/users/{user:username}', [UserController::class, 'index'])->name('users.index');
+
+Route::get('/posts/{post}/likes', [LikeController::class, 'index'])->name('posts.likes.index');
+Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
+Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
