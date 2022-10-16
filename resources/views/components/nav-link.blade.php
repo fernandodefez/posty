d19dd5a07ceb16ff6a659670262b546fab425ed0
Fernando <fernandodefez@outlook.com>
2022-10-16 13:49:35 -0500

    feat: change link style

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'block text-sm p-2 hover:bg-gray-100 text-blue-700 hover:text-blue-700 rounded md:bg-transparent transition duration-150 ease-in-out md:p-0 dark:text-white'
+            : 'block hover:bg-gray-100 p-2 text-sm md:hover:bg-transparent md:p-0 md:hover:text-blue-700 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
