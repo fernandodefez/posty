34492c33663e68fd7e31a610607822305c771496
Fernando <fernandodefez@outlook.com>
2022-10-16 13:51:54 -0500

    feat: add the current post to be edited.

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,38 +1,42 @@
 <x-app-layout>
-    <x-slot name="header">
-        <div class="flex justify-between py-4 items-center m-0">
-            <h2 class="font-bold text-xl text-gray-800 leading-tight py-2">
-                {{ __('Edit Post') }}
-            </h2>
-        </div>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <p class="p-0 mb-3">
-                        Post
-                    </p>
+    <div class="py-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 gap-8 grid grid-cols-1 md:grid-cols-2">
+            <div class="bg-transparent mb-8">
+                <x-post :post="$post"></x-post>
+            </div>
+            <div class="overflow-hidden shadow-sm rounded">
+                <div class="p-6 bg-white">
                     @auth
-                        <form action="{{ route('posts.update', $post) }}" method="POST" class="mb-5">
+                        <form action="{{ route('posts.update', $post) }}" method="POST">
                             @csrf
                             @method('PUT')
-                            <div class="mb-3">
-                                <label for="body" class="sr-only">
-                                    Body
-                                </label>
-                                <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100  border outline-0 transition-all focus:border-2 focus:border-blue-500 w-full p-4 rounded-lg @error('body') border-2 border-red-500 @enderror" placeholder="Post something!">{{ $post->body }}</textarea>
-                                @error('body')
-                                <div class="text-red-500 mt-2 text-sm">
+                            <div class="relative">
+                                <x-label for="title" :value="__('Title')" />
+                                <input id="title" type="text" name="title" value="{{ $post->title }}" placeholder="Title"
+                                       class="mt-2 mb-1.5 bg-gray-100 transition-all text-sm font-medium w-full py-2 px-2 border-2 outline-2 outline-blue-600 rounded shadow-sm border-gray-300 focus:border-blue-400 focus:ring focus:ring-blue-400 focus:ring-opacity-50
+                                         @error('title') border-2 bg-red-50 focus:border-red-400 focus:ring focus:ring-red-400 focus:ring-opacity-50 border-red-500 @enderror"
+                                />
+                                <div class="text-red-500 text-xs font-medium h-2">
+                                    @error('title')
                                     {{ $message }}
+                                    @enderror
                                 </div>
-                                @enderror
                             </div>
-                            <div>
-                                <button type="submit" class="bg-blue-500 text-white px-3 py-1 rounded font-semibold">
-                                    Edit
-                                </button>
+                            <div class="relative mt-4">
+                                <x-label for="body" :value="__('Body')" />
+                                <textarea name="body" id="body" cols="30" rows="8" placeholder="Post something!"
+                                          class="mt-2 bg-gray-100 transition-all text-sm font-medium w-full py-2 px-2 border-2 outline-2 outline-blue-600 rounded shadow-sm border-gray-300 focus:border-blue-400 focus:ring focus:ring-blue-400 focus:ring-opacity-50
+                                    @error('body') border-2 bg-red-50 focus:border-red-400 focus:ring focus:ring-red-400 focus:ring-opacity-50 border-red-500 @enderror">{{ $post->body }}</textarea>
+                                <div class="text-red-500 text-xs font-medium h-2">
+                                    @error('body')
+                                    {{ $message }}
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="mt-5">
+                                <x-button>
+                                    {{ __('Edit post') }}
+                                </x-button>
                             </div>
                         </form>
                     @endauth
