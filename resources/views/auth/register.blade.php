bc36775fee2dae209c06b96ff0156b650c07f7fa
Fernando <fernandodefez@outlook.com>
2022-10-16 13:55:15 -0500

    feat: change register inputs styles

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,8 +6,10 @@
             </a>
         </x-slot>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        <div class="h-6 mb-5 mt-2">
+            <!-- Validation Errors -->
+            <x-auth-validation-errors :errors="$errors" />
+        </div>
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
@@ -16,43 +18,62 @@
             <div>
                 <x-label for="name" :value="__('Name')" />
 
-                <x-input id="name" class="block mt-1 px-2 py-3 w-full" type="text" name="name" :value="old('name')" required autofocus
-                    placeholder="Enter name"/>
+                <input id="name" class="mt-1 bg-gray-100 transition-all text-sm font-medium w-full py-2 px-2 border-2 outline-2 outline-blue-600 rounded shadow-sm border-gray-300 focus:border-blue-400 focus:ring focus:ring-blue-400 focus:ring-opacity-50 @error('name') border-2 bg-red-50 focus:border-red-400 focus:ring focus:ring-red-400 focus:ring-opacity-50 border-red-500 @enderror" type="text" name="name" name="{{ old('name') }}" autofocus placeholder="Enter name"/>
+                <div class="text-red-500 text-xs font-normal h-4 mt-1">
+                    @error('name')
+                    {{ $message }}
+                    @enderror
+                </div>
             </div>
 
             <!-- Username -->
-            <div class="mt-4">
+            <div class="mt-3">
                 <x-label for="username" :value="__('Username')" />
 
-                <x-input id="username" class="block mt-1 px-2 py-3 w-full" type="text" name="username" :value="old('username')" required
-                         autofocus placeholder="Enter username"/>
+                <input id="username" class="mt-1 bg-gray-100 transition-all text-sm font-medium w-full py-2 px-2 border-2 outline-2 outline-blue-600 rounded shadow-sm border-gray-300 focus:border-blue-400 focus:ring focus:ring-blue-400 focus:ring-opacity-50 @error('username') border-2 bg-red-50 focus:border-red-400 focus:ring focus:ring-red-400 focus:ring-opacity-50 border-red-500 @enderror" type="text" name="username" value="{{ old('username') }}" autofocus placeholder="Enter username"/>
+                <div class="text-red-500 text-xs font-normal h-4 mt-1">
+                    @error('username')
+                    {{ $message }}
+                    @enderror
+                </div>
             </div>
 
             <!-- Email Address -->
-            <div class="mt-4">
+            <div class="mt-3">
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 py-2 px-3 w-full" type="email" name="email" :value="old('email')"
-                         required placeholder="Enter email"/>
+                <input id="email" class="mt-1 bg-gray-100 transition-all text-sm font-medium w-full py-2 px-2 border-2 outline-2 outline-blue-600 rounded shadow-sm border-gray-300 focus:border-blue-400 focus:ring focus:ring-blue-400 focus:ring-opacity-50 @error('email') border-2 bg-red-50 focus:border-red-400 focus:ring focus:ring-red-400 focus:ring-opacity-50 border-red-500 @enderror" type="email" name="email" value="{{ old('email') }}"
+                         placeholder="Enter email"/>
+                <div class="text-red-500 text-xs font-normal h-4 mt-1">
+                    @error('email')
+                    {{ $message }}
+                    @enderror
+                </div>
             </div>
 
             <!-- Password -->
-            <div class="mt-4">
+            <div class="mt-3">
                 <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 py-2 px-3 w-full"
-                         type="password"
-                         name="password"
-                         required autocomplete="new-password" placeholder="Enter password" />
+                <input id="password" class="mt-1 bg-gray-100 transition-all text-sm font-medium w-full py-2 px-2 border-2 outline-2 outline-blue-600 rounded shadow-sm border-gray-300 focus:border-blue-400 focus:ring focus:ring-blue-400 focus:ring-opacity-50 @error('password') border-2 bg-red-50 focus:border-red-400 focus:ring focus:ring-red-400 focus:ring-opacity-50 border-red-500 @enderror" type="password" name="password" autocomplete="new-password" placeholder="Enter password" />
+                <div class="text-red-500 text-xs font-normal h-4 mt-1">
+                    @error('password')
+                    {{ $message }}
+                    @enderror
+                </div>
             </div>
 
             <!-- Confirm Password -->
-            <div class="mt-4">
+            <div class="mt-3">
                 <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                <x-input id="password_confirmation" class="block mt-1 px-2 py-3 w-full"
-                         type="password"
-                         name="password_confirmation" required placeholder="Confirm password"/>
+                <input id="password_confirmation" class="mt-1 bg-gray-100 transition-all text-sm font-medium w-full py-2 px-2 border-2 outline-2 outline-blue-600 rounded shadow-sm border-gray-300 focus:border-blue-400 focus:ring focus:ring-blue-400 focus:ring-opacity-50 @error('password_confirmation') border-2 bg-red-50 focus:border-red-400 focus:ring focus:ring-red-400 focus:ring-opacity-50 border-red-500 @enderror"
+                         type="password" name="password_confirmation" placeholder="Confirm password"/>
+                <div class="text-red-500 text-xs font-normal h-4 mt-1">
+                    @error('password_confirmation')
+                    {{ $message }}
+                    @enderror
+                </div>
             </div>
 
             <div class="flex items-center justify-end mt-4">
