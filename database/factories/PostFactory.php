8daad529cf2e1003262869c459ae21dec6cf9f14
Fernando <fernandodefez@outlook.com>
2022-10-16 13:43:48 -0500

    chore: add post factory with fake data.

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(80),
+            'body' => $this->faker->text(400),
+            'picture' => $this->faker->imageUrl(3840,2160)
+        ];
+    }
+}
