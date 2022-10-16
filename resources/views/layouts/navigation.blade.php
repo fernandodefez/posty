a6808f1685525398e38e4913533b3ab7a25f8e29
Fernando <fernandodefez@outlook.com>
2022-10-16 13:50:01 -0500

    feat: change navigation layout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,52 +1,88 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
+<nav class="bg-white border-b border-gray-100 dark:bg-gray-900">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('home') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
-                        🏠 {{ __('Home') }}
-                    </x-nav-link>
-
-                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
-                        📜 {{ __('Posts') }}
-                    </x-nav-link>
-                </div>
-            </div>
-
-            @auth()
-            <div class="flex space-x-3">
-                <a href="{{ route('users.index', Auth::user()->username) }}" class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                    <div>
-                        🧑 {{ Auth::user()->name }}
+        <div class="w-full flex flex-wrap justify-between items-center mx-auto py-3 transition duration-150 ease-in-out">
+            <a href="{{ route('home') }}" class="flex items-center">
+                <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+            </a>
+            <div class="flex items-center md:order-2">
+                @auth()
+                    <button type="button" class="flex items-center mr-1 text-sm hover:bg-gray-100 rounded-full md:mr-0 focus:ring-2 focus:ring-gray-200" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                        <span class="font-medium text-sm text-gray-700 px-2">
+                            {{ Auth::user()->username }}
+                        </span>
+                        <img class="w-9 h-9 rounded-full" src="{{ Auth::user()->avatar }}" alt="user photo">
+                    </button>
+                    <div class="flex items-center md:order-2">
+                        <!-- Dropdown menu -->
+                        <div class="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+                            <div class="py-3 px-4">
+                                <span class="block text-sm text-gray-900 dark:text-white">
+                                    <a href="{{ route('users.index', Auth::user()->username) }}" class="flex items-center text-sm font-medium text-gray-600 hover:text-gray-800 hover:border-gray-400 focus:outline-none focus:text-gray-800 focus:border-gray-400 transition duration-150 ease-in-out">
+                                        <div>{{ Auth::user()->username }} </div>
+                                    </a>
+                                </span>
+                                <span class="block text-xs font-normal text-gray-400 truncate dark:text-gray-400">
+                                    {{ Auth::user()->email }}
+                                </span>
+                            </div>
+                            <ul class="py-1" aria-labelledby="user-menu-button">
+                                <li>
+                                    <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="">
+                                        <form method="POST" action="{{ route('logout') }}" class="p-0 m-0">
+                                            @csrf
+                                            <a href="{{ route('logout') }}" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                               onclick="event.preventDefault(); if (confirm('Are you sure you want to log out?'))  this.closest('form').submit();">
+                                                {{ __('Logout') }}
+                                            </a>
+                                        </form>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </a>
-                <!-- Log Out -->
-                <form method="POST" action="{{ route('logout') }}" class="flex items-center">
-                    @csrf
-                    <a href="{{ route('logout') }}" class="flex items-center text-sm font-medium text-red-500 hover:text-red-700 hover:border-red-300 focus:outline-none focus:text-red-700 focus:border-red-300 transition duration-150 ease-in-out" onclick="event.preventDefault(); if (confirm('Are you sure you want to log out?'))  this.closest('form').submit();  ">
-                        {{ __('Logout') }}
-                    </a>
-                </form>
+                @else
+                    <div class="flex items-center space-x-3">
+                        <a href="{{ route('login') }}" class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            Login
+                        </a>
+                        <a href="{{ route('register') }}" class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            Register
+                        </a>
+                    </div>
+                @endauth
+                <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                </button>
             </div>
-            @else
-                <div class="flex space-x-3">
-                    <a href="{{ route('login') }}" class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                        Login
-                    </a>
-                    <a href="{{ route('register') }}" class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                        Register
-                    </a>
-                </div>
-            @endauth
+            <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
+                <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <li>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
