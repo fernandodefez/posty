767516db7a0f6c3674241dd99120dd7b1f1a12f2
Fernando <fernandodefez@outlook.com>
2022-10-16 13:35:52 -0500

    feat: add verified middleware to like and dislike post.

--- app/Http/Controllers/Web/Post/Like/LikeController.php
+++ app/Http/Controllers/Web/Post/Like/LikeController.php
@@ -3,22 +3,26 @@
 namespace App\Http\Controllers\Web\Post\Like;
 
 use App\Http\Controllers\Controller;
+use App\Mail\PostLiked;
 use App\Models\Like;
 use App\Models\Post;
 use App\Services\LikeService;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class LikeController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth'])->only(['store', 'destroy']);
+        $this->middleware(['auth', 'verified'])->only(['store', 'destroy']);
     }
 
-    public function index(Post $post)
+    public function index(Post $post): View
     {
-        $likes = Like::with(['user'])->where('post_id', $post->id)->latest()->paginate(5);
+        $likes = Like::withoutTrashed()->with(['user'])->where('post_id', $post->id)->latest()->paginate(10);
+
         return view('posts.likes.index')->with([
             'post' => $post,
             'likes' => $likes
@@ -28,7 +32,7 @@ class LikeController extends Controller
     public function store(Post $post, Request $request, LikeService $likeService): RedirectResponse
     {
         $this->authorize('like', $post);
-        $likeService->create($post, $request->user());
+        $likeService->create($post, $request);
         return back();
     }
 
