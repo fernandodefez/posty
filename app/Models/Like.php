abbf605e35d5f633726dec42ee0014bdba6053b2
Fernando <fernandodefez@outlook.com>
2022-10-16 14:02:02 -0500

    feat: add delete_at to be fillable

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -5,13 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Like extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'user_id'
+        'user_id',
+        'delete_at'
     ];
 
     public function user(): BelongsTo
